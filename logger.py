cb3d6f6c75b8b8883c7034dbc178b6dd175ad87a
KozakaiAya <zju3150105246@outlook.com>
2019-08-29 19:12:41 -0400

    use current the script's path as log dir

--- logger.py
+++ logger.py
@@ -1,14 +1,16 @@
 import datetime
 import os
+import sys
 
 import utils
 
 class Logger():
     def __init__(self):
-        utils.safe_mkdir('./log/')
+        cur_path = sys.path[0]
+        utils.safe_mkdir(os.path.join(cur_path, 'log/')
 
     def log(self, content):
-        path = os.path.join('./log/', datetime.datetime.now().strftime("%Y%m%d"))
+        path = os.path.join(cur_path, 'log/', datetime.datetime.now().strftime("%Y%m%d"))
         utils.safe_mkdir(path)
         fn = os.path.join(path, datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.log')
         with open(fn, 'w') as f:
