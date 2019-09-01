8721db4d57265d93984183b503bbd3fdf274405a
KozakaiAya <zju3150105246@outlook.com>
2019-08-31 23:39:27 -0400

    fix bug in logger

--- logger.py
+++ logger.py
@@ -6,11 +6,11 @@ import utils
 
 class Logger():
     def __init__(self):
-        cur_path = sys.path[0]
-        utils.safe_mkdir(os.path.join(cur_path, 'log/'))
+        self.cur_path = sys.path[0]
+        utils.safe_mkdir(os.path.join(self.cur_path, 'log/'))
 
     def log(self, content):
-        path = os.path.join(cur_path, 'log/', datetime.datetime.now().strftime("%Y%m%d"))
+        path = os.path.join(self.cur_path, 'log/', datetime.datetime.now().strftime("%Y%m%d"))
         utils.safe_mkdir(path)
         fn = os.path.join(path, datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.log')
         with open(fn, 'w') as f:
